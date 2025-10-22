65212d8b4639b2c7399bb4e594f1538dd6d2691d
luay-software <su27lz@student.aau.dk>
2025-10-22 23:41:12 +0200

    Second commit

--- kryptering.c
+++ kryptering.c
@@ -1,24 +1,52 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(void) {
-    int key;
-    char text[10];
-    char crypt_text[10];
+int main(void) 
+{
+    int input, key;
+    char text[50];
+    char crypt_text[50];
 
-    printf("Enter the text you wish to encrypt: \n");
-    scanf("%s", text);
-    printf("Enter the key: \n");
-    scanf("%d", &key);
-    printf("Text: %s, key: %d\n", text, key);
+    printf("Press 1 if you wish to encrypt a text\n");
+    printf("Press 2 if you wish to decrypt an encrypted text\n");
+    scanf("%d", &input);
 
-    for (size_t i = 0; i < strlen(text); i++)
+    if (input == 1) 
     {
-        crypt_text[i] = text[i] + key; 
+        printf("Enter the text you wish to encrypt: \n");
+        scanf(" %[^\n]s",&text);
+        printf("Enter the key: \n");
+        scanf("%d", &key);
+        printf("Text: %s, key: %d\n", text, key);
+
+        int i = 0;
+        for (i; i < strlen(text); i++)
+        {
+            crypt_text[i] = text[i] + key; 
+        }
+        crypt_text[i] = '\0';
+
+        printf("Encrypted text: %s\n", crypt_text);
+    } else if (input == 2) 
+    {
+        printf("Enter the text you wish to decrypt: \n");
+        scanf(" %[^\n]s",&text);
+        printf("Enter the key: \n");
+        scanf("%d", &key);
+        printf("Text: %s, key: %d\n", text, key);
+
+        int i = 0;
+        for (i; i < strlen(text); i++)
+        {
+            crypt_text[i] = text[i] - key; 
+        }
+        crypt_text[i] = '\0';
+
+        printf("Decrypted text: %s\n", crypt_text);
+    } else
+    {
+        printf("Try again\n");
     }
     
-    printf("%s\n", crypt_text);
-    printf("%d\n", strlen(text));
-
     return 0;
 }
